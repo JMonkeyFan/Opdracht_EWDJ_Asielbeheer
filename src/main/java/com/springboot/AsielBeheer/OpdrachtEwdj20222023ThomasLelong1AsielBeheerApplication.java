a34aedd512fefc8abe24731dc2dea194ecf1f77a
Thomas Lelong <Thomas.Lelong@student.hogent.be>
2023-09-01 00:46:55 +0200

    First

---
+++ src/main/java/com/springboot/AsielBeheer/OpdrachtEwdj20222023ThomasLelong1AsielBeheerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.AsielBeheer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpdrachtEwdj20222023ThomasLelong1AsielBeheerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpdrachtEwdj20222023ThomasLelong1AsielBeheerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/AsielBeheer/OpdrachtEwdj20222023ThomasLelong1AsielBeheerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.AsielBeheer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpdrachtEwdj20222023ThomasLelong1AsielBeheerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
